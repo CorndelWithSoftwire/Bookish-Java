d5394d192713329437f98017825e79ef734e1cc8
Borbala Tobias <borbala.tobias@aexp.com>
2021-10-08 11:33:45 +0100

    fixed create table

--- bookish.sql
+++ bookish.sql
@@ -19,12 +19,20 @@ CREATE TABLE book (
     genre VARCHAR(255) NULL,
     summary VARCHAR(255) NULL,
     number_of_copies INT NOT NULL,
+    author_id INT NOT NULL,
     FOREIGN KEY (author_id) REFERENCES author (id)
 );
 
 CREATE TABLE copy_registry (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    book_id INT NOT NULL,
     FOREIGN KEY (book_id) REFERENCES book (id),
+    borrowed_by INT NOT NULL,
     FOREIGN KEY (borrowed_by) REFERENCES user (id),
     return_date VARCHAR(10)
 );
+
+-- DROP TABLE author;
+-- DROP TABLE book;
+-- DROP TABLE user;
+-- DROP TABLE copy_registry;
