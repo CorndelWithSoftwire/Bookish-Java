787a66f0a67a23e4469923f470de6da0af60cbee
vnguy4 <vanthoai.nguyen@aexp.com>
2021-10-08 14:56:56 +0100

    added user object

--- src/main/java/org/softwire/training/bookish/models/database/User.java
+++ src/main/java/org/softwire/training/bookish/models/database/User.java
@@ -1,4 +1,27 @@
 package org.softwire.training.bookish.models.database;
 
 public class User {
+    private int id;
+    private String forename;
+    private String surname;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
 }
