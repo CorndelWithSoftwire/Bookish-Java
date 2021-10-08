7ac34a44f2c9e68d3dcc09e1b5cf2b860eed6cc7
Borbala Tobias <borbala.tobias@aexp.com>
2021-10-08 15:00:25 +0100

    create copy registry object

---
+++ src/main/java/org/softwire/training/bookish/models/database/CopyRegistry.java
@@ -0,0 +1,47 @@
+package org.softwire.training.bookish.models.database;
+
+public class CopyRegistry {
+    private int id;
+    private int bookId;
+    private int borrowedBy;
+    private String returnDate;
+
+    public CopyRegistry(int id, int bookId, int borrowedBy, String returnDate) {
+        this.id = id;
+        this.bookId = bookId;
+        this.borrowedBy = borrowedBy;
+        this.returnDate = returnDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getBorrowedBy() {
+        return borrowedBy;
+    }
+
+    public void setBorrowedBy(int borrowedBy) {
+        this.borrowedBy = borrowedBy;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+}
