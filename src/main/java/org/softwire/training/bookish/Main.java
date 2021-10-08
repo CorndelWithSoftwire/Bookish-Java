1ab112d6c19a980c1b080655f28170825a430218
Amber Shand <amber.shand@aexp.com>
2021-10-08 15:04:05 +0100

    prints out books and its availabilty

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -15,6 +15,7 @@ public class Main {
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
         jdbcMethod(connectionString);
+        getBookCopies(connectionString);
         jdbiMethod(connectionString);
     }
 
@@ -32,6 +33,26 @@ public class Main {
             String technology = rs.getString("name");
             String url = rs.getString("logoUrl");
             System.out.println(technology + "'s logo URL: " + url);
+        }
+
+
+
+    }
+
+    private static void getBookCopies(String connectionString) throws SQLException {
+        System.out.println("JDBC method...");
+
+        // TODO: print out the details of all the books (using JDBC)
+        // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
+
+        Connection connection = DriverManager.getConnection(connectionString);
+        String query = "select * from book";
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        while(rs.next()) {
+            String title = rs.getString("title");
+            String numberOfCopies = rs.getString("number_of_copies");
+            System.out.println(title +  " has " + numberOfCopies + " copies available.");
         }
 
 
