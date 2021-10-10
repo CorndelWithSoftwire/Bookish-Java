209b15134b908e114f4700768b8826963deda22f
Max S-T <maxstantay@gmail.com>
2021-10-10 16:46:45 +0100

    correct some things in the db schema

--- DBSchema.sql
+++ DBSchema.sql
@@ -1,63 +1,58 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
 
 CREATE TABLE `Book` (
-    `BookID` int  NOT NULL ,
+    `BookId` int  NOT NULL ,
     `Title` varchar(255)  NOT NULL ,
-    `Authors` int  NOT NULL ,
     `Category` int  NOT NULL ,
-    `Created_at` date  NOT NULL ,
-    `Updated_at` date  NOT NULL ,
+    `CreatedAt` date  NOT NULL ,
+    `UpdatedAt` date  NOT NULL ,
     `Slug` varchar(255)  NOT NULL ,
-    `ISBN` varchar(255)  NULL ,
+    `Isbn` varchar(255)  NULL ,
     `Subtitle` varchar(255)  NULL ,
     `Subjects` varchar(255)  NULL ,
-    `Cover_photo_url` varchar(255)  NULL ,
+    `CoverPhotoUrl` varchar(255)  NULL ,
     PRIMARY KEY (
-        `BookID`
+        `BookId`
     )
 );
 
 CREATE TABLE `Copies` (
-    `BookID` int  NOT NULL ,
+    `CopyId` int  NOT NULL ,
+    `BookId` int  NOT NULL ,
     PRIMARY KEY (
-        `BookID`
+        `CopyId`
     )
 );
 
 CREATE TABLE `BookAuthor` (
-    `Author_pointer` int  NOT NULL ,
-    PRIMARY KEY (
-        `Author_pointer`
-    )
+    `Book` int  NOT NULL ,
+    `Author` int  NOT NULL 
 );
 
 CREATE TABLE `Authors` (
-    `AuthorID` int  NOT NULL ,
-    `First_name` varchar(32)  NOT NULL ,
-    `Last_name` varchar(32)  NOT NULL ,
+    `AuthorId` int  NOT NULL ,
+    `FirstName` varchar(32)  NOT NULL ,
+    `LastName` varchar(32)  NOT NULL ,
     PRIMARY KEY (
-        `AuthorID`
+        `AuthorId`
     )
 );
 
 CREATE TABLE `Categories` (
-    `CategoryID` int  NOT NULL ,
+    `CategoryId` int  NOT NULL ,
     `Category` varchar(32)  NULL ,
     PRIMARY KEY (
-        `CategoryID`
+        `CategoryId`
     )
 );
 
 CREATE TABLE `Users` (
     `Username` varchar(32)  NOT NULL ,
-    `Password_hash` varchar(32)  NOT NULL ,
+    `PasswordHash` varchar(32)  NOT NULL ,
     `Email` varchar(255)  NOT NULL ,
-    `Phone_number` long  NOT NULL ,
+    `PhoneNumber` long  NOT NULL ,
     PRIMARY KEY (
         `Username`
     )
@@ -71,34 +66,34 @@ CREATE TABLE `Librarians` (
 );
 
 CREATE TABLE `Borrows` (
-    `BorrowID` int  NOT NULL ,
-    `Borrowed_copy_ID` int  NOT NULL ,
+    `BorrowId` int  NOT NULL ,
+    `BorrowedCopyId` int  NOT NULL ,
     `Username` varchar(32)  NOT NULL ,
-    `Check_out_date` date  NOT NULL ,
-    `Check_in_date` date  NOT NULL ,
-    `Due_date` date  NOT NULL ,
+    `CheckOutDate` date  NOT NULL ,
+    `CheckInDate` date  NOT NULL ,
+    `DueDate` date  NOT NULL ,
     PRIMARY KEY (
-        `BorrowID`
+        `BorrowId`
     )
 );
 
-ALTER TABLE `Book` ADD CONSTRAINT `fk_Book_Authors` FOREIGN KEY(`Authors`)
-REFERENCES `BookAuthor` (`Author_pointer`);
+ALTER TABLE `Book` ADD CONSTRAINT `fk_Book_BookId` FOREIGN KEY(`BookId`)
+REFERENCES `BookAuthor` (`Book`);
 
 ALTER TABLE `Book` ADD CONSTRAINT `fk_Book_Category` FOREIGN KEY(`Category`)
-REFERENCES `Categories` (`CategoryID`);
+REFERENCES `Categories` (`CategoryId`);
 
-ALTER TABLE `Copies` ADD CONSTRAINT `fk_Copies_BookID` FOREIGN KEY(`BookID`)
-REFERENCES `Book` (`BookID`);
+ALTER TABLE `Copies` ADD CONSTRAINT `fk_Copies_CopyId` FOREIGN KEY(`CopyId`)
+REFERENCES `Borrows` (`BorrowedCopyId`);
 
-ALTER TABLE `Authors` ADD CONSTRAINT `fk_Authors_AuthorID` FOREIGN KEY(`AuthorID`)
-REFERENCES `BookAuthor` (`Author_pointer`);
+ALTER TABLE `Copies` ADD CONSTRAINT `fk_Copies_BookId` FOREIGN KEY(`BookId`)
+REFERENCES `Book` (`BookId`);
 
-ALTER TABLE `Users` ADD CONSTRAINT `fk_Users_Username` FOREIGN KEY(`Username`)
-REFERENCES `Librarians` (`Username`);
+ALTER TABLE `Authors` ADD CONSTRAINT `fk_Authors_AuthorId` FOREIGN KEY(`AuthorId`)
+REFERENCES `BookAuthor` (`Author`);
 
-ALTER TABLE `Borrows` ADD CONSTRAINT `fk_Borrows_Borrowed_copy_ID` FOREIGN KEY(`Borrowed_copy_ID`)
-REFERENCES `Copies` (`BookID`);
+ALTER TABLE `Librarians` ADD CONSTRAINT `fk_Librarians_Username` FOREIGN KEY(`Username`)
+REFERENCES `Users` (`Username`);
 
 ALTER TABLE `Borrows` ADD CONSTRAINT `fk_Borrows_Username` FOREIGN KEY(`Username`)
 REFERENCES `Users` (`Username`);
@@ -106,9 +101,9 @@ REFERENCES `Users` (`Username`);
 CREATE INDEX `idx_Book_Title`
 ON `Book` (`Title`);
 
-CREATE INDEX `idx_Authors_First_name`
-ON `Authors` (`First_name`);
+CREATE INDEX `idx_Authors_FirstName`
+ON `Authors` (`FirstName`);
 
-CREATE INDEX `idx_Authors_Last_name`
-ON `Authors` (`Last_name`);
+CREATE INDEX `idx_Authors_LastName`
+ON `Authors` (`LastName`);
 
