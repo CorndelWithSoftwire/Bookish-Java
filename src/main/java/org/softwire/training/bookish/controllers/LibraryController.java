89df1cb3acae29cd8b0971872d83c83632376bd8
vnguy4 <vanthoai.nguyen@aexp.com>
2021-10-11 15:40:43 +0100

    working on adding new book feature

--- src/main/java/org/softwire/training/bookish/controllers/LibraryController.java
+++ src/main/java/org/softwire/training/bookish/controllers/LibraryController.java
@@ -1,10 +1,12 @@
 package org.softwire.training.bookish.controllers;
 
 import org.softwire.training.bookish.models.database.Book;
+import org.softwire.training.bookish.models.database.Technology;
 import org.softwire.training.bookish.models.page.LibraryPageModel;
 import org.softwire.training.bookish.services.LibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
@@ -31,6 +33,14 @@ public class LibraryController {
         libraryPageModel.setBooks(allBooks);
 
         return new ModelAndView("library", "libraryModel", libraryPageModel);
+    }
+
+    @RequestMapping("/add-book")
+    RedirectView addBook(@ModelAttribute Book book) {
+
+        libraryService.addBook(book);
+
+        return new RedirectView("/library");
     }
 
     @RequestMapping("/sort-by-title")
