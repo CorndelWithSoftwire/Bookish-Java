573c0b6eeb0c9ac84a8fc37646a7fc921d308ec5
Max Stanford-Taylor <Max.StanfordTaylor@aexp.com>
2021-10-11 17:21:32 +0100

    start writing tests

---
+++ src/test/java/org/softwire/training/bookish/UserDbTest.java
@@ -0,0 +1,48 @@
+package org.softwire.training.bookish;
+
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.junit.jupiter.api.Test;
+import org.softwire.training.bookish.models.dao.UserDao;
+import org.softwire.training.bookish.models.database.User;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class UserDbTest {
+	@Test
+	void testInsert() {
+		Properties connProperties = new Properties();
+		connProperties.put("user", "root");
+		connProperties.put("password", "c7f/SGXS<80D1H/Iqf0PQp90@dicw(J?");
+		connProperties.setProperty("useSSL", "false");
+		Jdbi jdbi = Jdbi.create("jdbc:mysql://localhost:3306/bookish", connProperties);
+		jdbi.installPlugin(new SqlObjectPlugin());
+
+		Date date = new Date();
+
+		String username = "test" + date.getTime();
+		String testPassword = "testPassword";
+		String email = "test@example.com";
+		String phone = "07756545321";
+
+		User testUser = new User();
+
+		testUser.setUsername(username);
+		testUser.setPasshashFromString(testPassword);
+		testUser.setEmail(email);
+		testUser.setPhone(phone);
+
+		testUser.insertUserToDatabase(jdbi);
+
+		User assertionUser = new User();
+		assertionUser.getUserFromDatabase(jdbi, username);
+
+		assertThat(assertionUser.getUsername()).isEqualTo(username);
+		assertThat(assertionUser.getEmail()).isEqualTo(email);
+		assertThat(assertionUser.getPhone()).isEqualTo(phone);
+	}
+}
