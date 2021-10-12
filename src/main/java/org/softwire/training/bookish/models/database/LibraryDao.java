8c4953252bdf5258d81561f95a195bac147086d9
vnguy4 <vanthoai.nguyen@aexp.com>
2021-10-12 15:15:08 +0100

    add book function and delete function per book

--- src/main/java/org/softwire/training/bookish/models/database/LibraryDao.java
+++ src/main/java/org/softwire/training/bookish/models/database/LibraryDao.java
@@ -4,6 +4,7 @@ import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.Define;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 import java.util.List;
 
@@ -11,4 +12,11 @@ public interface LibraryDao {
     @SqlQuery("SELECT * FROM book ORDER BY <column>")
     @RegisterBeanMapper(value = Book.class)
     public List<Book> sort(@Define("column") String column);
+
+    @SqlUpdate("INSERT INTO book (title, ISBN, published_date, publisher, genre, number_of_copies, author_id) " +
+            "VALUES (:title, :ISBN, :published_date, :publisher, :genre, :number_of_copies, :author_id)")
+    void insertBook(@Bind("title") String title, @Bind("ISBN") int ISBN, @Bind("published_date") String published_date,
+                    @Bind("publisher") String pulisher, @Bind("genre") String genre, @Bind("number_of_copies") int number_of_copies,
+                    @Bind("author_id") int author_id);
+
 }
