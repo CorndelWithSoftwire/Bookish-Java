e916b1008320272738de6a9d73bb74bbdc15452e
tbrough54 <90706267+tbrough54@users.noreply.github.com>
2021-10-13 15:44:26 +0100

    Revert "Buttons created for each author and links to the library page"

--- src/main/java/org/softwire/training/bookish/models/database/AuthorDao.java
+++ src/main/java/org/softwire/training/bookish/models/database/AuthorDao.java
@@ -1,10 +1,8 @@
 package org.softwire.training.bookish.models.database;
 
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
-import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.Define;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
-import org.jdbi.v3.sqlobject.statement.UseRowReducer;
 
 import java.util.List;
 
@@ -17,11 +15,4 @@ public interface AuthorDao {
     @RegisterBeanMapper(value = Author.class)
     List<Author> sortReverse(@Define("column") String column);
 
-    @SqlQuery(" SELECT author.name AS aname, author.id AS aid, author.place_of_birth as aplace_of_birth, " +
-            " book.id, book.title, book.ISBN, book.published_date, book.publisher, book.genre, book.number_of_copies, book.author_id " +
-            " FROM book JOIN author ON book.author_id = author.id where author.id LIKE :authorId")
-    @RegisterBeanMapper(value = Author.class, prefix = "a")
-    @RegisterBeanMapper(value = Book.class)
-    @UseRowReducer(BookAuthorReducer.class)
-    List<Author> listAuthorAndBooks(@Bind("authorId") String title);
 }
