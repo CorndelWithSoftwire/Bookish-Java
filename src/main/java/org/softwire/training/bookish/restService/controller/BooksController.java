34ed89eb1d5633515470dabb7f77b2fb513f9d43
Pedro-D13 <p.afonsosd1994@gmail.com>
2021-10-14 16:48:38 +0100

    ApiEnd Point now allows you to get the details of a single book

--- src/main/java/org/softwire/training/bookish/restService/controller/BooksController.java
+++ src/main/java/org/softwire/training/bookish/restService/controller/BooksController.java
@@ -5,29 +5,27 @@ import org.softwire.training.bookish.models.database.Book;
 import org.softwire.training.bookish.models.database.BookDict;
 import org.softwire.training.bookish.models.database.Books;
 import org.softwire.training.bookish.populateDB.PopulateDB;
-import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class BooksController {
+    Jdbi jdbi = PopulateDB.createJdbiConnection();
 
     @GetMapping("books/{page}")
     List<BookDict> getAllBookNames(@PathVariable(value = "page", required = false) int page) {
-        Jdbi jdbi = PopulateDB.createJdbiConnection();
-        return new Books().getBooksList(jdbi, page);
+        return new Books().getBooksList(this.jdbi, page);
     }
 
-//    @GetMapping("books/{id}")
-//    Book getBookById(@PathVariable(value = "id",required = true) int id ){
-//        return new Book().get
-//    }
+    @GetMapping("book/{id}")
+    Book getBookById(@PathVariable(value = "id", required = true) int id) {
+        return new Book().getBookById(this.jdbi, id);
+    }
 
 
 }
