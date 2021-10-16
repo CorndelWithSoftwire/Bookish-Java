14735d378e39c410a7525e2cee6ca5048419eb16
Borbala Tobias <borbala.tobias@aexp.com>
2021-10-16 14:08:14 +0100

    solve the author name finder problem

--- src/main/java/org/softwire/training/bookish/controllers/BookSplashController.java
+++ src/main/java/org/softwire/training/bookish/controllers/BookSplashController.java
@@ -6,9 +6,7 @@ import org.softwire.training.bookish.models.page.BookSplashPageModel;
 import org.softwire.training.bookish.services.BookSplashService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -35,6 +33,7 @@ public class BookSplashController {
 
     @RequestMapping("/edit-book")
     RedirectView editBook(@ModelAttribute Book book) {
+
         try{
             bookSplashService.editBook(book);
         } catch (Exception e) {
@@ -45,12 +44,23 @@ public class BookSplashController {
     }
 
     @RequestMapping("/find-author")
-    ModelAndView findAuthor(@RequestParam String id) {
-        Author author = bookSplashService.getAuthor(Integer.parseInt(id));
+    @ResponseBody
+    public String findAuthor(@RequestParam int id) {
+        Author author;
+        String authorName;
 
-        BookSplashPageModel bookSplashPageModel = new BookSplashPageModel();
-        bookSplashPageModel.setAuthor(author);
+        if (id == 0) {
+            authorName = "No author with this name";
+        } else {
+            author = bookSplashService.getAuthor(id);
+            if (author == null) {
+                authorName = "No author with this name";
+            } else {
+                authorName = author.getName();
+            }
 
-        return new ModelAndView("book-splash", "bookSplashModel", bookSplashPageModel);
+        }
+
+        return authorName;
     }
 }
