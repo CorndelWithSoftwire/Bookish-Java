d93875472c35af71372fee7d40205e6475b0a78d
vnguy4 <vanthoai.nguyen@aexp.com>
2021-10-19 11:47:54 +0100

    added error handling on required fields and logging

--- src/main/java/org/softwire/training/bookish/services/UserService.java
+++ src/main/java/org/softwire/training/bookish/services/UserService.java
@@ -5,6 +5,8 @@ import org.softwire.training.bookish.models.database.Book;
 import org.softwire.training.bookish.models.database.User;
 import org.softwire.training.bookish.models.database.UserDao;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -18,48 +20,56 @@ public class UserService extends DatabaseService {
         );
     }
 
-        public void borrow(int book_id, int user_id) {
+    public void borrow(int book_id, int user_id) {
 
-        List<Book> currentInventory = this.getBorrowList(user_id);
-        for (Book book: currentInventory) {
-            if (book.getId() == book_id) {
-                System.out.println("deny borrow more than once");
-                return;
-            }
+        // check that book id exists in user's booklist
+        HashMap<Integer, Book> bookHashMap = this.getBookHashMap(user_id);
+        Book book = bookHashMap.get(book_id);
+        // quick break is already in inventory
+        if (book != null) {
+            return;
         }
 
         jdbi.useHandle(handle ->
                 handle.createUpdate("UPDATE book " +
-                "SET number_of_copies = GREATEST(0, number_of_copies - 1) " +
-                "WHERE book.id = :book_id")
+                                "SET number_of_copies = GREATEST(0, number_of_copies - 1) " +
+                                "WHERE book.id = :book_id")
                         .bind("book_id", book_id)
                         .execute()
         );
 
-            jdbi.useHandle(handle ->
-                    handle.createUpdate("INSERT INTO copy_registry (book_id, borrowed_by, return_date) VALUES " +
-                                    "(:book_id, :user_id, '2021-12-31');")
-                            .bind("book_id", book_id)
-                            .bind("user_id", user_id)
-                            .execute()
-            );
+        jdbi.useHandle(handle ->
+                handle.createUpdate("INSERT INTO copy_registry (book_id, borrowed_by, return_date) VALUES " +
+                                "(:book_id, :user_id, '2021-12-31');")
+                        .bind("book_id", book_id)
+                        .bind("user_id", user_id)
+                        .execute()
+        );
     }
 
     public void returnBook(int book_id, int user_id) {
-            jdbi.useHandle(handle ->
-                    handle.createUpdate("DELETE FROM copy_registry WHERE book_id = :book_id and borrowed_by = :user_id ;")
-                            .bind("book_id", book_id)
-                            .bind("user_id", user_id)
-                            .execute()
-            );
+        // check that book id exists in user's booklist
+        HashMap<Integer, Book> bookHashMap = this.getBookHashMap(user_id);
+        Book book = bookHashMap.get(book_id);
+        // quick break if book id is not in inventory
+        if (book == null) {
+            return;
+        }
 
-            jdbi.useHandle(handle ->
-                    handle.createUpdate("UPDATE book " +
-                            "SET number_of_copies = GREATEST(0, number_of_copies + 1) " +
-                            "WHERE id = :book_id;")
-                            .bind("book_id", book_id)
-                            .execute()
-            );
+        jdbi.useHandle(handle ->
+                handle.createUpdate("DELETE FROM copy_registry WHERE book_id = :book_id and borrowed_by = :user_id ;")
+                        .bind("book_id", book_id)
+                        .bind("user_id", user_id)
+                        .execute()
+        );
+
+        jdbi.useHandle(handle ->
+                handle.createUpdate("UPDATE book " +
+                                "SET number_of_copies = GREATEST(0, number_of_copies + 1) " +
+                                "WHERE id = :book_id;")
+                        .bind("book_id", book_id)
+                        .execute()
+        );
     }
 
 
@@ -69,6 +79,15 @@ public class UserService extends DatabaseService {
 
         return jdbi.withExtension(
                 UserDao.class, dao -> dao.booksBorrowedByUserID(id));
+    }
+
+    public HashMap<Integer, Book> getBookHashMap(int id) {
+        List<Book> bookList= this.getBorrowList(id);
+        HashMap<Integer, Book> bookHashMap = new HashMap<>();
+        for (Book book: bookList) {
+            bookHashMap.put(book.getId(), book);
+        }
+        return bookHashMap;
     }
 
     public List<Book> getAvailableBooksToBorrow() {
