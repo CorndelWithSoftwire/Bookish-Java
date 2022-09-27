e1548f18dc5673479ec20892fc2fdbd6a1392574
Will Squires <w.r.squires@hotmail.co.uk>
2022-09-27 14:00:51 +0100

    Use of both libraries to select from database

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -1,19 +1,21 @@
 package org.softwire.training.bookish;
 
+import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.statement.SqlParser;
+import org.softwire.training.bookish.models.database.Book;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
 
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         String hostname = "localhost";
-        String database = "bookish";
-        String user = "bookish";
-        String password = "bookish";
+        String database = "Library";
+        String user = "root";
+        String password = "%20";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
         jdbcMethod(connectionString);
@@ -27,8 +29,10 @@ public class Main {
         // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
 
         Connection connection = DriverManager.getConnection(connectionString);
-
-
+        Statement statement = connection.createStatement();
+        String query = "SELECT * FROM Books";
+        ResultSet results = statement.executeQuery(query);
+        System.out.println(results);
 
     }
 
@@ -40,6 +44,8 @@ public class Main {
         // Use the "Book" class that we've created for you (in the models.database folder)
 
         Jdbi jdbi = Jdbi.create(connectionString);
+        List<Book> books = jdbi.withHandle(handle -> {return handle.createQuery("SELECT * FROM Book").mapTo(Book.class).list();});
+        System.out.println(books);
 
 
 
--- src/main/java/org/softwire/training/bookish/models/database/Book.java
+++ src/main/java/org/softwire/training/bookish/models/database/Book.java
@@ -1,5 +1,21 @@
 package org.softwire.training.bookish.models.database;
 
 public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String ISBN;
+    private String genre;
+    private int copies;
 
+
+    public Book(){
+    }
+
+    public int getID(){return this.id;}
+    public String getTitle(){return this.title;}
+    public String getAuthor(){return this.author;}
+    public String getISBN(){return this.ISBN;}
+    public String getGenre(){return this.genre;}
+    public int getCopies(){return this.copies;}
 }
